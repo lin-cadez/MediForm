e8067fe9481b499dce066580cb022e0f3c51cc38
Jaka Černetič <jaka.cernetic@students.vegova.si>
2024-10-30 23:16:09 +0100

    idk man

--- src/selector/selector.tsx
+++ src/selector/selector.tsx
@@ -4,21 +4,33 @@ import Logo from "../logo.jpg";
 
 export default function Selector() {
 	return (
-		<div className="selector-page">
+		<div>
 			<nav>
 				<div className="logo">
 					<img src={Logo} alt="logo" />
 				</div>
 				<div className="about">
-					<NavLink to="/about">
-						O nas
-					</NavLink>
+					<NavLink to="/about">O nas</NavLink>
 				</div>
 			</nav>
-			<h1>izbirnik</h1>
-			<NavLink to="/checklist">
-				<button>Seznam opravil</button>
-			</NavLink>
+			<main>
+				<h1>Seznami</h1>
+				<p>Izberi seznam, ki ga želiš izpolniti.</p>
+				<div className="seznami">
+					<NavLink to="/checklist" className="seznam">
+						<h2>Seznam opravil</h2>
+						<p>Seznam opravil, ki jih moraš opraviti.</p>
+					</NavLink>
+					<NavLink to="/checklist" className="seznam">
+						<h2>Nakupovalni seznam</h2>
+						<p>Seznam stvari, ki jih moraš kupiti.</p>
+					</NavLink>
+					<NavLink to="/checklist" className="seznam">
+						<h2>Seznam želja</h2>
+						<p>Seznam stvari, ki si jih želiš.</p>
+					</NavLink>
+				</div>
+			</main>
 		</div>
 	);
 }
