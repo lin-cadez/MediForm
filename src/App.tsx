86be52315ae2cc5161d6400643cb031d69e6333c
Jaka Černetič <jaka.cernetic@gmail.com>
2024-11-24 21:53:28 +0100

    neki

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,33 @@ import Checklist from "./checklist/checklist";
 import Selector from "./selector/selector";
 import About from "./about/about";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { register } from "register-service-worker";
 
 function App() {
+  register('/service-worker.js', {
+    ready() {
+      console.log('App is being served from cache by a service worker.');
+    },
+    registered() {
+      console.log('Service worker has been registered.');
+    },
+    cached() {
+      console.log('Content has been cached for offline use.');
+    },
+    updatefound() {
+      console.log('New content is downloading.');
+    },
+    updated() {
+      console.log('New content is available; please refresh.');
+    },
+    offline() {
+      console.log('No internet connection found. App is running in offline mode.');
+    },
+    error(error) {
+      console.error('Error during service worker registration:', error);
+    }
+  });
+
   return (
     <div>
       <Router>
